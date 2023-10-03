98d30dbc53aaa6b08bf883a9041ebcd454e1196a
Angel <80134042+Ninthharp8@users.noreply.github.com>
2023-10-03 17:11:42 -0600

    agregue el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './Componentes/login/login.component';
+import { SignupComponent } from './Componentes/signup/signup.component';
+const routes: Routes = [
+  {path: 'login', component:LoginComponent},
+  {path: 'signup', component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
