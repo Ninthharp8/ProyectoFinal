63ff14b428df757229c385a2f60ddafaadacf14e
Angel <80134042+Ninthharp8@users.noreply.github.com>
2023-10-04 23:10:04 -0600

    as

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,13 @@ const routes: Routes = [
   {path: 'login', component:LoginComponent},
   {path: 'signup', component:SignupComponent},
   {path: '', component: PrincipalComponent}
-
+]
 
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
 export class AppRoutingModule { }
+  
