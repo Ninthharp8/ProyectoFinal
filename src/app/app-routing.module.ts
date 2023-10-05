648cd0626a450777acffc2127cf08075f37eb096
Angel <80134042+Ninthharp8@users.noreply.github.com>
2023-10-04 23:08:28 -0600

    se resolvieron las colisiones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { LoginComponent } from './Componentes/login/login.component';
 import { SignupComponent } from './Componentes/signup/signup.component';
-const routes: Routes = [
-  {path: 'login', component:LoginComponent},
-  {path: 'signup', component:SignupComponent}
-=======
 import { PrincipalComponent } from './Componentes/principal/principal.component';
 
 const routes: Routes = [
+  {path: 'login', component:LoginComponent},
+  {path: 'signup', component:SignupComponent},
   {path: '', component: PrincipalComponent}
->>>>>>> 94d101d132200586bb268c4041180f4979947d83
-];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
