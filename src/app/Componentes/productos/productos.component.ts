967e3d2a9aaed5cb8092a63cb45d2341e1e07523
Angel <80134042+Ninthharp8@users.noreply.github.com>
2023-10-09 09:26:49 -0600

    aplique boostrap al componente principal

--- src/app/Componentes/productos/productos.component.ts
+++ src/app/Componentes/productos/productos.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductosService } from 'src/app/Servicios/productos.service';
+import { Producto } from 'src/app/Interfaces/producto';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {
+
+
+export class ProductosComponent implements OnInit {
+
+  productos: Producto[]=[];
+
+  constructor(private ProductsService: ProductosService){}
+
+  ngOnInit() {
+    this.ProductsService.getProductos().subscribe(productos => {
+      this.productos = productos;});
+  }
+
+
+
+
+
 
 }
--- src/app/Servicios/productos.service.ts
+++ src/app/Servicios/productos.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Producto } from '../Interfaces/producto';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  private apiUrl = 'URL_DE_TU_API';
+
+
+  constructor(private http:HttpClient) {}
+
+  getProductos(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(this.apiUrl);
+  }
+
+
+  
+
 }
